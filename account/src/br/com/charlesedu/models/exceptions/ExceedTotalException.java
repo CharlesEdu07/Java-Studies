e3cd97c0577402828cc02cd4695da28768cc4186
unknown <charleseduardofaria@gmail.com>
2023-07-25 17:56:19 -0300

    Adicionando projeto exceptions (account)

---
+++ account/src/br/com/charlesedu/models/exceptions/ExceedTotalException.java
@@ -0,0 +1,23 @@
+package br.com.charlesedu.models.exceptions;
+
+public class ExceedTotalException extends Exception {
+    public ExceedTotalException(String message) {
+        super(message);
+    }
+
+    public ExceedTotalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ExceedTotalException(Throwable cause) {
+        super(cause);
+    }
+
+    public ExceedTotalException() {
+    }
+
+    public ExceedTotalException(String message, Throwable cause, boolean enableSuppression,
+            boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
